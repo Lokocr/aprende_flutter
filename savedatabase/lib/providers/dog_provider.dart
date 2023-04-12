d540f9006a4f36b3806c7b630514a01590235876
Jose Cartin <jose.cartin@tiendamia.com>
2023-04-11 20:40:46 -0600

    Se actualiza los comentarios a español

--- savedatabase/lib/providers/dog_provider.dart
+++ savedatabase/lib/providers/dog_provider.dart
@@ -12,10 +12,12 @@ class DogProvider extends ChangeNotifier {
   int age = 0;
   int id = 0;
 
+  // Inicializamos la base de datos.
   Future<void> initDatabase() async {
     openDatabase(
       join(await getDatabasesPath(), 'doggie_database.db'),
       onCreate: (db, version) {
+        // Cuando la base de datos es creada por primera vez, crea una tabla para almacenar dogs.
         db.execute(
           "CREATE TABLE dogs(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER)",
         );
@@ -24,7 +26,7 @@ class DogProvider extends ChangeNotifier {
     );
   }
 
-  // Getting a reference to the database
+  // Obtenemos la instancia de base de datos.
   Future<Database> getDatabase() async {
     return openDatabase(
       join(await getDatabasesPath(), 'doggie_database.db'),
@@ -36,19 +38,20 @@ class DogProvider extends ChangeNotifier {
     try {
       var db = await getDatabase();
 
-      // validate if name is empty
+      // Validamos si el valor en la variables name es vacio
       if (name.isEmpty) {
         throw Exception('Name is empty');
       }
 
-      // validate if age is empty
+      // Validamos si el valor en la variables age es 0
       if (age == 0) {
         throw Exception('Age is empty');
       }
 
-      // add name, age and id to newDog variable
+      // Agregamos los valores a una nueva instancia del modelo Dog
       newDog = Dog(name: name, age: age);
 
+      // Insertamos el nuevo modelo en la base de datos
       await db.insert(
         'dogs',
         newDog.toMap(),
@@ -64,7 +67,7 @@ class DogProvider extends ChangeNotifier {
     }
   }
 
-  // Reset all values
+  // Reseteamos los valores de las variables
   Future<void> resetValues() async {
     name = '';
     age = 0;
@@ -72,13 +75,14 @@ class DogProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  // get all dogs
+  // Obtenemos todos los registros de la base de datos
   Future<void> getDogs() async {
     final Database db = await getDatabase();
 
-    // get all dogs from database
+    // Obtenemos una lista de Map<String, dynamic> de la base de datos de la tabla de dogs
     final List<Map<String, dynamic>> maps = await db.query('dogs');
 
+    // Convertimos la List<Map<String, dynamic> en una List<Dog>.
     dogs = List.generate(maps.length, (i) {
       return Dog(
         id: maps[i]['id'],
@@ -90,7 +94,7 @@ class DogProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  // Remove dog from database
+  // Eliminamos un registro de la base de datos
   Future<void> removeDog(int id) async {
     try {
       final db = await getDatabase();
@@ -108,7 +112,7 @@ class DogProvider extends ChangeNotifier {
     getDogs();
   }
 
-  // validate if exist table
+  // Validmaos si la tabla existe
   Future<bool> isTableExist(String tableName) async {
     var db = await getDatabase();
     var res = await db.rawQuery(
